1c2df131e2b4d5f5243b892c658e35583a34c31f
Oleksii Astrakhantsev <92253303+alexeiastrakh@users.noreply.github.com>
2023-12-06 11:51:26 +0200

    Add files via upload

---
+++ GraphQL/Types/CurrentBalanceQueryGraphType.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using GraphQL.WebApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL.WebApi.GraphQL.Types
+{
+    class CurrentBalanceQueryGraphType : ObjectGraphType<AgentList>
+    {
+        public CurrentBalanceQueryGraphType()
+        {
+            Name = "AgentList";
+            //Field(x => x.Id, type: typeof(IdGraphType)).Description("Customer Id");
+            Field(x => x.currentBalance).Description("currentBalance");
+        }
+    }
+}
---
+++ GraphQL/Types/IdentityQueryGraphType.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using GraphQL.WebApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL.WebApi.GraphQL.Types
+{
+    class IdentityQueryGraphType : ObjectGraphType<AgentList>
+    {
+        public IdentityQueryGraphType()
+        {
+            Name = "AgentList";
+            //Field(x => x.Id, type: typeof(IdGraphType)).Description("Customer Id");
+            Field(x => x.identityId).Description("identity Id");
+    }
+    }
+}
